c3ef451d1fa345d06dfce626b65c5c6ece45a484
Alex Rujan <alexrujan3@gmail.com>
2025-10-08 16:33:42 +0300

    some .env changes

--- util/api.js
+++ util/api.js
@@ -1,6 +1,7 @@
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjE2MzUzYzA5YzNlZTQ3MTAyNGU5ZTBjMWNiZDhlNyIsIm5iZiI6MTcxMzAyMzM1Mi45ODIsInN1YiI6IjY2MWFhOTc4OTgyZjc0MDE2MzQ2YTI1OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZY03XoHzOtq6AjHoRBU9rcY2A-Zyw_ca_gJc86XGfog";
+import dotenv from "dotenv";
+dotenv.config();
 
+const API_KEY = process.env.API_KEY;
 
 export const options = {
   method: "GET",
